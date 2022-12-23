1efe8c3e94ca1cc8478ec180ae2fe06187a7ae78
Erik HR <writeerikh@gmail.com>
2022-12-23 12:24:51 +0700

    todo:add p_key

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const dotenv = require("dotenv");
 // const { db, encrypt } = require("./config");
 
 const vttController = require("./routes/voice-to-text");
-const tcController = require("./routes/text-completion");
 
 const app = express();
 
@@ -13,7 +12,6 @@ app.use(express.json());
 dotenv.config();
 // db();
 
-app.use("/textCompletion", tcController);
 app.use("/voice-tt", vttController);
 
 app.listen(2309, () => {
--- routes/voice-to-text.js
+++ routes/voice-to-text.js
@@ -1,6 +1,16 @@
 const express = require("express");
-
 const router = express.Router();
+
+const { Configuration, OpenAIApi } = require("openai");
+
+const config = new Configuration({
+  //   organization: "YOUR_ORG_ID",
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(config);
+
+let url = "https://api.openai.com/v1/completions";
 
 router.get("/vttTest", (req, res) => {
   console.log("voice to text hit");
