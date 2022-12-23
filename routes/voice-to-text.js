e216ee1249203448e2ad42a661e61b4bc05d7084
Erik HR <writeerikh@gmail.com>
2022-12-23 14:20:49 +0700

    response in json

--- routes/voice-to-text.js
+++ routes/voice-to-text.js
@@ -6,59 +6,33 @@ const { Configuration, OpenAIApi } = require("openai");
 
 const config = new Configuration({
   //   organization: "YOUR_ORG_ID",
-  apiKey: '',
+  apiKey: "",
 });
 
 const openai = new OpenAIApi(config);
 
-const pleaseRespond = async() => {
-  const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: "\nQ: How old is Jay-Z?\nA:",
-    temperature: 0,
-    max_tokens: 100,
-    top_p: 1,
-    frequency_penalty: 0.0,
-    presence_penalty: 0.0,
-    stop: ["\n"],
-  });
-  
-  console.log(response.data)
-};
+router.post("/testResponse", async (req, res) => {
+  const { prompt } = req.body;
+  const pleaseRespond = async (data) => {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `\nQ: ${data}\nA:`,
+      temperature: 0,
+      max_tokens: 100,
+      top_p: 1,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+      stop: ["\n"],
+    });
 
-pleaseRespond()
+    if (response.data) {
+      res.status(201).json({
+        results: response.data.choices[0].text,
+      });
+    }
+  };
 
-
-
-
-
-
-// let URL = "https://api.openai.com/v1/completions";
-
-// router.post("/vttTest", async (req, res) => {
-//   const { model, prompt, temperature, max_tokens } = req.body;
-
-//   try {
-//     const response = await openai.listEngines({
-//       model: model,
-//       prompt: prompt,
-//       n: 1,
-//       temperature: 1,
-//       max_tokens: max_tokens,
-//     });
-
-//     const results = response.data.data;
-//     console.log("results" +  results);
-//     console.log("response" +  response);
-//     console.log("working");
-
-//     res.status(200).json({
-//       success: true,
-//       data: results,
-//     });
-//   } catch (err) {
-//     console.log(err);
-//   }
-// });
+  pleaseRespond(prompt);
+});
 
 module.exports = router;
