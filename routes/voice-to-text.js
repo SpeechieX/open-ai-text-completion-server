4d207c443e17e979e1d664e612fd1da65ac75ff6
djtoler <84260551+djtoler@users.noreply.github.com>
2022-12-23 01:53:38 -0500

    prompt woking

--- routes/voice-to-text.js
+++ routes/voice-to-text.js
@@ -6,34 +6,59 @@ const { Configuration, OpenAIApi } = require("openai");
 
 const config = new Configuration({
   //   organization: "YOUR_ORG_ID",
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: '',
 });
 
 const openai = new OpenAIApi(config);
 
-let URL = "https://api.openai.com/v1/completions";
+const pleaseRespond = async() => {
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: "\nQ: How old is Jay-Z?\nA:",
+    temperature: 0,
+    max_tokens: 100,
+    top_p: 1,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+    stop: ["\n"],
+  });
+  
+  console.log(response.data)
+};
 
-router.post("/vttTest", async (req, res) => {
-  const { model, prompt, temperature, max_tokens } = req.body;
+pleaseRespond()
 
-  try {
-    const response = await openai.listEngines({
-      model: model,
-      prompt: prompt,
-      n: 1,
-      temperature: 1,
-      max_tokens: max_tokens,
-    });
 
-    const results = response.data.data;
 
-    res.status(200).json({
-      success: true,
-      data: results,
-    });
-  } catch (err) {
-    console.log(err);
-  }
-});
+
+
+
+// let URL = "https://api.openai.com/v1/completions";
+
+// router.post("/vttTest", async (req, res) => {
+//   const { model, prompt, temperature, max_tokens } = req.body;
+
+//   try {
+//     const response = await openai.listEngines({
+//       model: model,
+//       prompt: prompt,
+//       n: 1,
+//       temperature: 1,
+//       max_tokens: max_tokens,
+//     });
+
+//     const results = response.data.data;
+//     console.log("results" +  results);
+//     console.log("response" +  response);
+//     console.log("working");
+
+//     res.status(200).json({
+//       success: true,
+//       data: results,
+//     });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
 
 module.exports = router;
